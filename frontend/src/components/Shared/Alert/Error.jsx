59e8d5f504c4fe7d0155575eb104f818d71e99b2
Nadeera Sampath <Nadeera3784@gmail.com>
2022-07-17 18:34:21 +1000

    feat : error alert ui added

---
+++ frontend/src/components/Shared/Alert/Error.jsx
@@ -0,0 +1,9 @@
+function Error({message}) {
+
+    return (
+        <div className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+           {message}
+        </div>
+    )
+}
+export default Error;
