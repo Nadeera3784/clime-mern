7eb7dc68c70a7630e38a705154ed74a5bd4a6142
Nadeera Sampath <Nadeera3784@gmail.com>
2022-07-17 18:36:46 +1000

    feat : alert success ui added

---
+++ frontend/src/components/Shared/Alert/Success.jsx
@@ -0,0 +1,9 @@
+function Success({message}) {
+
+    return (
+        <div class="p-4 mb-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800" role="alert">
+           {message}
+        </div>
+    )
+}
+export default Success;
