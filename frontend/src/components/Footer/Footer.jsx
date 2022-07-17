71f60d3da96ed1735988eb86b9d639d964e1df35
Nadeera Sampath <Nadeera3784@gmail.com>
2022-07-17 18:33:34 +1000

    feat : footer ui added

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+    return (
+        <footer  className="flex flex-none items-center bg-white">
+            <div className="text-center flex flex-col md:text-left md:flex-row md:justify-between text-sm container xl:max-w-7xl mx-auto px-4 lg:px-8">
+            <div className="pt-4 pb-1 md:pb-4">
+                <a href="" target="_blank" className="font-medium text-indigo-600 hover:text-indigo-400">Clime</a> © 2020
+            </div>
+            <div className="pb-4 pt-1 md:pt-4 inline-flex items-center justify-center">
+                <span>Crafted with</span>
+                <svg className="hi-solid hi-heart inline-block w-4 h-4 mx-1 text-red-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd"/></svg>
+                <span>by <a href="" target="_blank" className="font-medium text-indigo-600 hover:text-indigo-400">Clime</a></span>
+            </div>
+            </div>
+      </footer>
+    )
+}
+
+export default Footer;
