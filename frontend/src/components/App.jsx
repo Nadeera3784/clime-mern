93ead3983618ad8cbe2142ba7cbf275953e3f0a9
Nadeera Sampath <Nadeera3784@gmail.com>
2022-07-17 18:29:42 +1000

    feat : app ui added

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,16 +1,23 @@
-import {Routes, Route } from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 
-import Home              from './Home/Home';
-import Login             from './Login/Login';
-import Register             from './Register/Register';
+import Home          from '@/components/Home/Home';
+import Login         from '@/components/Login/Login';
+import Register      from '@/components/Register/Register';
+import Dashboard     from '@/components/Dashboard/Dashboard';
+import RequireAuth   from '@/components/Navigation/RequireAuth';
 
 function App() {
 
   return (
     <Routes>
-       <Route path="/" exact element={<Home/>}/>
-       <Route path="/login" exact element={<Login/>}/>
-       <Route path="/register" exact element={<Register/>}/>
+      <Route path="/" exact element={<Home/>}/>
+      <Route path="/login" exact element={<Login/>}/>
+      <Route path="/register" exact element={<Register/>}/>
+      <Route path="/dashboard" exact element={
+          <RequireAuth>
+             <Dashboard/>
+          </RequireAuth>
+      }/>      
     </Routes>
   )
 }
