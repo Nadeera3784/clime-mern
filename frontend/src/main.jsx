b60132801948c83cf56d267e99b84645c8ce3eff
Nadeera Sampath <Nadeera3784@gmail.com>
2022-07-17 18:28:47 +1000

    feat : root store context added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
-import App from './components/App'
+import App from './components/App';
+import AppProvider from './store/AppContext';
 import './assets/css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+  <AppProvider>
+    <BrowserRouter>
+       <App/>
+    </BrowserRouter>
+  </AppProvider>
 )
