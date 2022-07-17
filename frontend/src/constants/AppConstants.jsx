c1ab45934a4f5041f9b7fd4c6b4ed74785451414
Nadeera Sampath <Nadeera3784@gmail.com>
2022-07-17 18:33:07 +1000

    feat : constants file added

---
+++ frontend/src/constants/AppConstants.jsx
@@ -0,0 +1,8 @@
+const AppConstants = {
+    AUTH_TOKEN                :  "access_token",
+    AUTH_REFRESH_TOKEN        :  "refresh_token",
+    AUTH_SET_USER             :  "AUTH_SET_USER",
+    GET_WEATHER_REPORTS       :  "GET_WEATHER_REPORTS"
+}
+
+export default AppConstants;
